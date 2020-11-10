e98d8d538caeed8620291a0cc7ca5239c56023dd
2sharjain <f20170201@hyderabad.bits-pilani.ac.in>
2020-11-10 18:02:46 +0530

    Made Index

--- makeIndex.py
+++ makeIndex.py
@@ -1,8 +1,14 @@
 from nltk.stem import WordNetLemmatizer
 import nltk
 import os
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+
+
 lematizer = WordNetLemmatizer()
-for i in range(0, 1):
+corpus = []
+N = 5
+for i in range(0, N):
     file_name = './dataset/rfc'+str(i)+'.txt'
     f = open(file_name, mode='r')
     doc = f.read()
@@ -11,5 +17,19 @@ for i in range(0, 1):
     stop_words = set(nltk.corpus.stopwords.words('english'))
     words = [lematizer.lemmatize(
         token) for token in tokens if token.isalnum() and token not in stop_words]
-    words = list(set(words))
-    print(words)
+    # print(words)
+    s = ""
+    for word in words:
+        s += word
+        s += " "
+    # print(s)
+    corpus.append(s)
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+
+dictionary = vectorizer.get_feature_names()
+file_index = ['doc' + str(i) for i in range(0, N)]
+count_df = pd.DataFrame(data=X.toarray(), index=file_index, columns=dictionary)
+
+print(count_df.head())
